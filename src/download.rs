60618e114d1e681d27b4e7e79e124b877ba4379b
olejodd10 <67640511+olejodd10@users.noreply.github.com>
2021-07-31 14:35:18 +0200

    Added reponse code eprintln and access_token argument to download functions

--- src/download.rs
+++ src/download.rs
@@ -1,9 +1,9 @@
 use std::path::{Path, PathBuf};
 use std::io::Write;
-use curl::easy::Easy;
+use curl::easy::{Easy, List};
 
 
-pub fn download_file(file_url: &str, out_path: &Path) -> Result<f64, Box<dyn std::error::Error>> {
+pub fn download_file(file_url: &str, out_path: &Path, access_token: Option<&str>) -> Result<f64, Box<dyn std::error::Error>> {
     
     let mut out_file = std::fs::File::create(&out_path).expect("Error creating out_file");
 
@@ -13,16 +13,23 @@ pub fn download_file(file_url: &str, out_path: &Path) -> Result<f64, Box<dyn std
         out_file.write_all(data).expect("Error writing data");
         Ok(data.len())
     })?;
-    // let mut list = List::new();
-    // list.append("Authorization: Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==").unwrap();
-    // easy.http_headers(list).unwrap();
+
+    if let Some(access_token) = access_token {
+        let mut list = List::new();
+        let autorization_header = format!("Authorization: Basic {}", access_token);
+        list.append(&autorization_header).unwrap();
+        easy.http_headers(list).unwrap();
+    }
 
     easy.perform()?;
+
+    eprintln!("Response code: {}", easy.response_code().unwrap());
+
     Ok(easy.download_size()?)
 }
 
 
-pub fn download_and_unzip(file_url: &str, out_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+pub fn download_and_unzip(file_url: &str, out_path: &Path, access_token: Option<&str>) -> Result<f64, Box<dyn std::error::Error>> {
     
     let out_path = PathBuf::from(out_path); //Må gjøre sånn her så en &Path ikke borrowes inn i closuren under
 
@@ -32,7 +39,17 @@ pub fn download_and_unzip(file_url: &str, out_path: &Path) -> Result<(), Box<dyn
         zip_extract::extract(std::io::Cursor::new(data), &out_path, true).expect("Unzipping error");
         Ok(data.len())
     })?;
+
+    if let Some(access_token) = access_token {
+        let mut list = List::new();
+        let autorization_header = format!("Authorization: Bearer {}", access_token);
+        list.append(&autorization_header).unwrap();
+        easy.http_headers(list).unwrap();
+    }
+
+    eprintln!("Response code: {}", easy.response_code().unwrap());
+
     easy.perform()?;
 
-    Ok(())
+    Ok(easy.download_size()?)
 }
